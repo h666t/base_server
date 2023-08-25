b98ddee02d2e3eeebcaae15027ce5cad47e9449f
root <root@DESKTOP-T1ADIRC.localdomain>
2023-08-25 23:35:11 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ app.use(async (ctx, next)=>{
       // notImplemented: () => '不支持当前请求所需要的功能',
       // methodNotAllowed: () => '不支持的请求方式'
   }));
-  app.listen(3000);
-  console.log('开始监听3000端口');
+  app.listen(80);
+  console.log('开始监听80端口');
   
 })();
